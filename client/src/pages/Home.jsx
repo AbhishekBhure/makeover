56ef752bc342be5942ca8d5134f269f94656bbf7
abhi <abhiab7411@gmail.com>
2023-12-20 23:46:52 +0530

    modified Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="">
+      <h1 className="font-primary text-3xl">Home</h1>
+      <p className="font-secondary">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. In optio
+        molestiae dicta vero ratione quibusdam consequuntur accusantium vel a
+        esse, ullam recusandae fugit quis non. Corrupti sapiente ipsa distinctio
+        unde?.
+      </p>
+    </div>
+  );
 };
 
 export default Home;
