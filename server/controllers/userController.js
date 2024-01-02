c0271d5f370398aa069addf3b30bf0529e367ec5
abhi <abhiab7411@gmail.com>
2024-01-02 13:01:47 +0530

    feature: added getAllUsers, singleUser

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,3 +1,35 @@
-export const getUsers = (req, res) => {
-  res.send("Hi users");
+import User from "../models/userModel.js";
+
+export const getAllUsers = async (req, res, next) => {
+  try {
+    const users = await User.find({});
+    const userLength = users.length;
+    res.status(200).json({ users, userLength });
+  } catch (error) {
+    next(error);
+  }
+};
+
+//Get a Single User
+export const singleUser = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id);
+    res.status(200).json(user);
+  } catch (error) {
+    next(error);
+  }
+};
+
+//Update a user
+export const updateUser = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const updatedUser = await User.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    res.status(201).json(updatedUser);
+  } catch (error) {
+    next(error);
+  }
 };
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,8 +1,8 @@
 import express from "express";
-import { getUsers } from "../controllers/userController.js";
+import { getAllUsers, singleUser } from "../controllers/userController.js";
 
 const router = express.Router();
 
-router.get("/", getUsers);
+router.get("/", getAllUsers).get("/:id", singleUser);
 
 export default router;
